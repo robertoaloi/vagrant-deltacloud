2b31069649af50e27c11e18758d69b0613457746
Andras Horvath <andras.horvath@erlang-solutions.com>
2013-04-10 17:28:07 +0200

    build, fix and install DC gem manually
    `gem install deltacloud' would fail with dependency mismatch:
        deltacloud-core requires
            excon < 0.17.0, >= 0.14.2
            fog >= 1.4.0
        fog---since 4 April '13---requires excon (~> 0.20)
    so patch the gemspec to use the previous version of fog

--- cookbooks/deltacloud/recipes/default.rb
+++ cookbooks/deltacloud/recipes/default.rb
@@ -1,13 +1,35 @@
-%w{ruby1.9.1-full g++ libxml2 libxml2-dev libxslt1.1 libxslt1-dev sqlite3 libsqlite3-dev make}.each do |pkg|
+%w{ruby1.9.1-full g++ libxml2 libxml2-dev libxslt1.1 libxslt1-dev sqlite3 libsqlite3-dev make patch}.each do |pkg|
   package pkg do
     action :install
   end
 end
 
-%w{thin sinatra rack-accept rest-client sinatra-content-for nokogiri deltacloud-core require_relative}.each do |pkg|
+%w{thin sinatra rack-accept rest-client sinatra-content-for nokogiri}.each do |pkg|
   gem_package pkg do
     action :install
   end
+end
+
+$DC_VER = '1.1.2'
+$dc_src_url = "http://xenia.sote.hu/ftp/mirrors/www.apache.org/deltacloud/stable/deltacloud-core-#{$DC_VER}.tgz"
+$CWD = "/home/vagrant"
+$dc_tgz = "#{$CWD}/dc.tar.gz"
+
+remote_file $dc_tgz do
+    source  $dc_src_url
+    mode 00644
+end
+
+bash "patch and install deltacloud" do
+    user "root"
+    cwd ::File.dirname($dc_tgz)
+    code <<-EOF
+        tar xzf #{$dc_tgz}
+        cd deltacloud-core-#{$DC_VER}/
+        patch </vagrant/deltacloud-core.gemspec.diff
+        gem build deltacloud-core.gemspec
+        gem install --no-rdoc --no-ri deltacloud-core-#{$DC_VER}.gem
+    EOF
 end
 
 service "deltacloud" do
